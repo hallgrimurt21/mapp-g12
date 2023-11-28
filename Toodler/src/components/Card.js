5189d9435cf4c45ffc34802b9f1b00c6c3fa87e1
HalliS2o <hallist@hey.com>
2023-11-28 14:45:56 +0000

    fixed card

--- Toodler/src/components/Card.js
+++ Toodler/src/components/Card.js
@@ -1,35 +1,55 @@
 import React, { useState } from "react"
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, View, Text } from "react-native"
+import Checkbox from "@material-ui/core/Checkbox"
 
 export default function Card(props) {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>{props.children}</Text>
-            <Checkbox />
+            <View style={styles.row}>
+                <Text style={styles.text}>{props.children}</Text>
+                <Checkbox style={styles.check} />
+            </View>
+            <Text style={styles.description}>Your description here</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: 120,
-        width: 300,
-        backgroundColor: "coral",
+        height: 80,
+        width: 350,
+        backgroundColor: "white",
         alignItems: "center",
         justifyContent: "space-between",
         flexDirection: "row",
         borderColor: "black", // color of the border
-        borderWidth: 2, // width of the border
-        borderRadius: 10,
+        borderWidth: 1, // width of the border
+        borderRadius: 5,
         padding: 10,
+        marginBottom: 10,
+        boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+        fontFamily: "Roboto",
+        flexDirection: "column",
     },
-
     text: {
-        color: "white",
-        fontSize: 25,
+        color: "black",
+        fontSize: 16,
         fontWeight: "bold",
         textShadowRadius: 10,
         textShadowColor: "white",
         marginRight: 10,
+    },
+    check: {
+        color: "black",
+        fontSize: 100,
+        fontWeight: "lighter",
+    },
+    description: {
+        color: "grey",
+        fontSize: 15,
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
     },
 })
