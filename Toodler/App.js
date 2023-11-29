629f366d294eb7d5cd5c22198952f747ce3ab253
HalliS2o <hallist@hey.com>
2023-11-29 09:45:10 +0000

    removed app.js junk and added drop down list

--- Toodler/App.js
+++ Toodler/App.js
@@ -1,41 +1,5 @@
 import React from "react"
-import { StyleSheet, View, Text } from "react-native"
-import { StatusBar } from "expo-status-bar"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import Card from "./src/components/card/Card"
-import data from "./src/resources/data.json"
-import { storeData, getX } from "./src/resources/storage"
-import { useEffect } from "react"
 
 export default function App() {
-    useEffect(() => {
-        storeData(data)
-    }, [])
-
-    getX("tasks").then((res) => console.log(res))
-
-    return (
-        <View style={styles.container}>
-            {data.tasks.map((task, index) => (
-                <Card
-                    name={task.name}
-                    description={task.description}
-                    key={index}
-                    id={task.id}
-                    boardId={task.boardId}
-                    isFinished={task.isFinished}
-                />
-            ))}
-            <StatusBar style="auto" />
-        </View>
-    )
+    return <Main />
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FEFCFF",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-})
