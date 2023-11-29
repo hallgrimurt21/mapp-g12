3ef157d04e58ffe7c1fb61d66f912d368c5fa9e9
HalliS2o <hallist@hey.com>
2023-11-29 11:24:49 +0000

    made board select for single board

--- Toodler/src/components/BoardSelect.js
+++ Toodler/src/components/BoardSelect.js
@@ -4,5 +4,9 @@ import { FlatList, Text } from "react-native"
 import data from "../resources/data.json"
 
 export default function BoardSelect() {
+    const boardId = 2 // replace with the ID of the board you want to display
+
+    const board = data.boards.find((board) => board.id === boardId)
+
     return <Lister board={board} />
 }
--- Toodler/src/views/Main/index.js
+++ Toodler/src/views/Main/index.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { View, TouchableOpacity, Text } from "react-native"
 import HomeScreen from "../../components/Homescreen"
+import BoardSelect from "../../components/BoardSelect"
 
-function Main ({ navigation: { navigate } }) {
+function Main({ navigation: { navigate } }) {
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <TouchableOpacity onPress={() => { navigate('Board') }}>
-                    <Text>board</Text>
-                </TouchableOpacity>
-            <HomeScreen />
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <TouchableOpacity
+                onPress={() => {
+                    navigate("Board")
+                }}
+            >
+                <Text>board</Text>
+            </TouchableOpacity>
+            <BoardSelect />
         </View>
     )
 }
