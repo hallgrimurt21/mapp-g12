2c035c91b28449eb495c2e67ae55680bc427fbaf
hallgrimurt21 <hallgrimurth@gmail.com>
2023-11-29 12:11:34 +0000

    navigating in BoardList

--- Toodler/src/components/BoardList/index.js
+++ Toodler/src/components/BoardList/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import styles from "./styles"
+import { useNavigation } from "@react-navigation/native"
 import data from "../../resources/data.json"
 import { Text, ScrollView, TouchableOpacity } from "react-native"
 
 const BoardList = () => {
+    const { navigate } = useNavigation();
     return (
         <ScrollView style={styles.container} >
             {/* list of boards */}
             {data.boards.map((board, index) => (
-                <TouchableOpacity key={board.id} style={styles.itemWithBorder}>
+                <TouchableOpacity key={board.id} style={styles.itemWithBorder} onPress={() => { navigate("Main") }}>
                     <Text style={styles.item}>{board.name}</Text>
                 </TouchableOpacity>
             ))}
