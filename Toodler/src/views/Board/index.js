9a911ec7320385a16f4d13ca0221ae4da499509a
hallgrimurt21 <hallgrimurth@gmail.com>
2023-11-29 15:56:04 +0000

    fix merge error

--- Toodler/src/views/Board/index.js
+++ Toodler/src/views/Board/index.js
@@ -1,23 +1,57 @@
 import React from "react"
-import { View, TouchableOpacity, Text } from "react-native"
+import {
+    View,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Dimensions,
+} from "react-native"
 import BoardSelect from "../../components/BoardSelect"
+import { navajo, smoke } from "../../styles/colors"
+import { deviceWidth } from "../../styles/deviceWidth"
+import { Image } from "expo-image"
 
-function Main({ route, navigation: { navigate } }) {
-    const { id } = route.params;
+function Board({ route,  navigation: { navigate } }) {
+    const {id} = route.params
     return (
-        <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
+        <View style={styles.viewer}>
+            <BoardSelect ider={id} />
             <TouchableOpacity
+                style={styles.button}
                 onPress={() => {
-                    navigate("Main")
+                    navigate("Board")
                 }}
             >
-                <Text>board</Text>
+                <Image
+                    source={require("../../components/Images/Hamburger.png")}
+                    style={styles.buttonText}
+                />
             </TouchableOpacity>
-            <BoardSelect ider={id} />
         </View>
     )
 }
 
-export default Main
+export default Board
+
+const styles = StyleSheet.create({
+    viewer: {
+        backgroundColor: smoke,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+    },
+
+    buttonText: {
+        width: 30,
+        height: 30,
+        alignSelf: "center",
+    },
+    button: {
+        height: 50,
+        width: deviceWidth * 1,
+        borderTopColor: "black",
+        borderTopWidth: 1,
+        justifyContent: "center",
+    },
+})
--- Toodler/src/views/Main/index.js
+++ Toodler/src/views/Main/index.js
@@ -3,29 +3,11 @@ import { View, Text } from "react-native"
 import styles from "./styles"
 import BoardList from "../../components/BoardList"
 
-function Main({ navigation: { navigate } }) {
-    return (
-        <View style={styles.viewer}>
-            <BoardSelect ider={2} />
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                    navigate("Board")
-                }}
-            >
-                <Image
-                    source={require("../../components/Images/Hamburger.png")}
-                    style={styles.buttonText}
-                />
-            </TouchableOpacity>
-        </View>
-    )
-}
-const Board = ({ navigation: { navigate } }) => (
+const Main = ({ navigation: { navigate } }) => (
     <View style={styles.container}>
         <Text style={styles.title}>Boards</Text>
         <BoardList />
     </View>
 )
 
-export default Board
+export default Main
