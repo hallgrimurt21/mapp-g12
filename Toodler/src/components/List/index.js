b8da6c05387d1f74df0c96165d011baef0fee1cb
HalliS2o <hallist@hey.com>
2023-11-30 13:22:39 +0000

    added useEffect and useState

--- Toodler/src/components/List/index.js
+++ Toodler/src/components/List/index.js
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { View, Text, Button, ScrollView } from "react-native"
-import { getX, get1X, changeX, addX } from "../Functions/storage"
+import { getX, get1X, changeX, addX } from "../../Functions/storage"
+import { getTasksByList } from "../../Functions/Manager"
 import Card from "../Card"
 import { shadows } from "../../styles/shadows"
 import { BlurView } from "expo-blur"
 import hexToRgb from "../../Functions/hexToRgb"
 import styles from "./styles"
+import data from "../../resources/data.json"
 
 function List({ list }) {
-    const data = getX("tasks")
-    const cards = data.filter((task) => task.listId === list.id)
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        getTasksByList(list.id).then((tasks) => {
+            setCards(tasks)
+        })
+    }, [list.id])
 
     return (
         <View style={[styles.container, shadows.mediumShadow]}>
