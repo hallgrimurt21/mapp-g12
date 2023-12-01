737bc8a4c8d0b71d216d9de61fc54f728c52bf0d
HalliS2o <hallist@hey.com>
2023-12-01 09:52:05 +0000

    finished modal fixes and fixed ids

--- Toodler/src/components/Modal/styles.js
+++ Toodler/src/components/Modal/styles.js
@@ -4,22 +4,23 @@ import { deviceHeight, deviceWidth } from "../../styles/deviceWidth"
 export default StyleSheet.create({
     modal: {
         alignItems: "center",
-        justifyContent: "flex-end",
+        justifyContent: "flex-start",
+        marginTop: deviceHeight * 0.13,
+        backgroundColor: "white",
+        width: deviceWidth,
+        marginLeft: -deviceWidth * 0.005,
+        marginBottom: -deviceHeight * 0.2,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
     },
     body: {
         alignItems: "center",
-        justifyContent: "flex-start",
-        width: deviceWidth,
-        backgroundColor: "white",
-        borderBottomLeftRadius: 10,
-        borderBottomRightRadius: 10,
+        justifyContent: "center",
         marginBottom: 0,
-        flexGrow: 0.6,
         overflow: "hidden",
         paddingBottom: 10,
     },
     empty: {
-        marginBottom: -deviceHeight * 0.2,
         backgroundColor: "white",
     },
 })
