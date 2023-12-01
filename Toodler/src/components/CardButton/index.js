a1e3076a70dcf08b6cf8e5b6f63887517a68b4a7
HalliS2o <hallist@hey.com>
2023-12-01 19:35:41 +0000

    more cardButton work

--- Toodler/src/components/CardButton/index.js
+++ Toodler/src/components/CardButton/index.js
@@ -1,11 +1,41 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef, useEffect, useState } from "react"
 import { Animated, Pressable } from "react-native"
 import Card from "../Card"
+import { changeTask } from "../../Functions/Manager"
 
-function CardButton({ info }) {
+function CardButton({ info: initialInfo }) {
+    const [info, setInfo] = useState(initialInfo)
+    const fadeAnim = useRef(new Animated.Value(1)).current
+
+    const handlePressIn = () => {
+        Animated.timing(fadeAnim, {
+            toValue: 0.5,
+            duration: 200,
+            useNativeDriver: true,
+        }).start()
+    }
+
+    const handlePressOut = () => {
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 200,
+            useNativeDriver: true,
+        }).start()
+        setInfo((prevInfo) => {
+            const updatedInfo = {
+                ...prevInfo,
+                isFinished: !prevInfo.isFinished,
+            }
+            changeTask(updatedInfo)
+            return updatedInfo
+        })
+    }
+
     return (
-        <Pressable>
-            <Card info={info} />
+        <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}>
+            <Animated.View style={{ opacity: fadeAnim }}>
+                <Card info={info} />
+            </Animated.View>
         </Pressable>
     )
 }
