c700df4c55b74ab2fe96d7dd7b7a97737674fc21
Ingi Kr <ingik21@ru.is>
2023-12-01 13:10:05 +0000

    AddListReadyBoy

--- Toodler/src/components/Modals/AddListModal/index.js
+++ Toodler/src/components/Modals/AddListModal/index.js
@@ -2,34 +2,36 @@ import React from "react"
 import {
     TextInput,
     SafeAreaView,
-    Pressable,
     Text,
     View,
     TouchableOpacity,
 } from "react-native"
 import Modal from "../../Modal"
 import styles from "./styles"
-import grey from "../../../styles/colors"
+import { colors } from "../../../styles/colors"
 import { shadows } from "../../../styles/shadows"
+import { Picker } from '@react-native-picker/picker';
 
 const AddListModal = ({ isOpen, closeModal, onModalClose }) => {
     const [name, onChangeName] = React.useState("")
-    const [Color, onChangeColor] = React.useState("")
+    const [color, onChangeColor] = React.useState("")
+    const [colorName, setColorName] = React.useState("");
+    const [isPickerVisible, setPickerVisible] = React.useState(false)
     const titleStyle = [styles.title, shadows.smallShadow]
     const modalStyle = [styles.modal, shadows.smallShadow]
     const buttonStyle = [styles.Button, shadows.smallShadow]
     const inputStyle = [styles.input, shadows.smallShadow]
 
     const handleClose = () => {
-        if (name === "") {
+        if (name === "" || color === "") {
             onChangeName("")
             onChangeColor("")
             closeModal()
         } else {
-            onModalClose(name, Color)
+            onModalClose(name, color)
             closeModal()
             onChangeName("") // clear the name input
-            onChangeColor("") // clear the Color input
+            onChangeColor("") // clear the color input
         }
     }
     return (
@@ -43,14 +45,26 @@ const AddListModal = ({ isOpen, closeModal, onModalClose }) => {
                         style={inputStyle}
                         onChangeText={onChangeName}
                         placeholder={"Name"}
-                        placeholderTextColor={grey}
+                        placeholderTextColor={colors.grey}
                     />
-                    <TextInput
-                        style={inputStyle}
-                        onChangeText={onChangeColor}
-                        placeholder={"Color(optional)"}
-                        placeholderTextColor={grey}
-                    />
+                    <TouchableOpacity onPress={() => setPickerVisible(true)}>
+                        <Text style={inputStyle}>{colorName || "color" }</Text>
+                    </TouchableOpacity>
+                    {isPickerVisible && (
+                        <Picker
+                            selectedValue={color}
+                            onValueChange={(itemValue, itemIndex) => {
+                                onChangeColor(itemValue);
+                                setColorName(Object.keys(colors)[itemIndex]);
+                                setPickerVisible(false);
+                            }}
+                            
+                        >
+                            {Object.keys(colors).map((key) => (
+                                <Picker.Item label={key} value={colors[key]} key={key} />
+                            ))}
+                        </Picker>
+                    )}
                     <TouchableOpacity
                         style={buttonStyle}
                         activeOpacity={0.5}
