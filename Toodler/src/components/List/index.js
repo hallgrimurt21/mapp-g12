1870b08560bb598b78d450d71b762c1505bbcb56
Ingik21 <ingik21@ru.is>
2023-12-02 14:37:14 +0000

    listInProgress

--- Toodler/src/components/List/index.js
+++ Toodler/src/components/List/index.js
@@ -1,120 +1,142 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import {
-    View,
-    Text,
-    Button,
-    ScrollView,
-    LayoutAnimation,
-    Platform,
-    UIManager,
-} from "react-native"
-import { addTask, getTasksByList } from "../../Functions/Manager"
-import { shadows } from "../../styles/shadows"
-import { BlurView } from "expo-blur"
-import hexToRgb from "../../Functions/hexToRgb"
-import styles from "./styles"
-import AddCardModal from "../Modals/AddCardModal"
-import Card from "../Card"
-import CardButton from "../CardButton"
+  View,
+  Text,
+  Button,
+  ScrollView,
+  LayoutAnimation,
+  Platform,
+  UIManager,
+} from "react-native";
+import { addTask, changeList, getTasksByList } from "../../Functions/Manager";
+import { shadows } from "../../styles/shadows";
+import { BlurView } from "expo-blur";
+import hexToRgb from "../../Functions/hexToRgb";
+import styles from "./styles";
+import AddCardModal from "../Modals/AddCardModal";
+import EditListModal from "../Modals/EditListModal";
+import Card from "../Card";
+import CardButton from "../CardButton";
 
 function List({ list, onDelete }) {
-    if (
-        Platform.OS === "android" &&
-        UIManager.setLayoutAnimationEnabledExperimental
-    ) {
-        UIManager.setLayoutAnimationEnabledExperimental(true)
-    }
+  if (
+    Platform.OS === "android" &&
+    UIManager.setLayoutAnimationEnabledExperimental
+  ) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
 
-    ///////// opening modal and setting cards /////////
-    const [isAddModalOpen, setIsAddModalOpen] = useState(false)
-    const [cards, setCards] = useState([])
-    const handleDeleteList = () => {
-        onDelete(list.id);
-      };
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [cards, setCards] = useState([]);
 
-    //////// For adding a task ////////
-    useEffect(() => {
-        getTasksByList(list.id).then((tasks) => {
-            setCards(tasks)
-        })
-    }, [list.id])
+  const handleDeleteList = () => {
+    onDelete(list.id);
+  };
 
-    ///// Deleting a task /////
-    const handleDelete = (id) => {
-        setCards(cards.filter((card) => card.id !== id))
-    }
+  useEffect(() => {
+    getTasksByList(list.id).then((tasks) => {
+      setCards(tasks);
+    });
+  }, [list.id]);
 
-    ////////// add task to asyncStorage and get tasks from asyncStorage+update //////////
-    function addTaskAndGetTasks(task) {
-        addTask(task)
-            .then(() => {
-                getTasksByList(list.id)
-                    .then((tasks) => {
-                        LayoutAnimation.configureNext(
-                            LayoutAnimation.Presets.easeInEaseOut,
-                        )
-                        setCards(tasks)
-                    })
-                    .catch((error) => {
-                        console.error("Error getting tasks: ", error)
-                    })
-            })
-            .catch((error) => {
-                console.error("Error adding task: ", error)
-            })
-    }
-    return (
-        <View style={[styles.container, shadows.mediumShadow]}>
-            <BlurView
-                style={[
-                    {
-                        backgroundColor: hexToRgb(list.color, 0.3),
-                        borderRadius: 5,
-                    },
-                ]}
-            >
-                <View style={[styles.titler]}>
-                    <Text style={styles.text}>{list.name}</Text>
-                </View>
-                <View style={[styles.container, shadows.mediumShadow]}>
-                {/* ... */}
-                 <Button title="Delete List" onPress={handleDeleteList} />
-                 </View>
-                <ScrollView style={styles.carder}>
-                    {cards.map((card) => (
-                        ////////// Card item as a button //////////
-                        <CardButton
-                            key={card.id}
-                            info={card}
-                            onDelete={handleDelete} //////// Deleting a task ////////
-                        />
-                    ))}
+  const handleDelete = (id) => {
+    setCards(cards.filter((card) => card.id !== id));
+  };
 
-                    <View style={styles.adder}>
-                        <Button
-                            style={styles.butRad}
-                            title="Add Task"
-                            onPress={() => {
-                                setIsAddModalOpen(true)
-                            }}
-                        />
-                    </View>
-                </ScrollView>
-                <AddCardModal
-                    isOpen={isAddModalOpen}
-                    closeModal={() => setIsAddModalOpen(false)}
-                    onModalClose={(name, description) => {
-                        addTaskAndGetTasks({
-                            name: name,
-                            description: description,
-                            listId: list.id,
-                        })
-                        // log the name and description
-                    }}
-                />
-            </BlurView>
+  function addTaskAndGetTasks(task) {
+    addTask(task)
+      .then(() => {
+        getTasksByList(list.id)
+          .then((tasks) => {
+            LayoutAnimation.configureNext(
+              LayoutAnimation.Presets.easeInEaseOut
+            );
+            setCards(tasks);
+          })
+          .catch((error) => {
+            console.error("Error getting tasks: ", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Error adding task: ", error);
+      });
+  }
+
+  const handleEditModalOpen = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const handleEditModalClose = (name, color) => {
+    // Update the list information here
+    // You can perform any necessary actions with the updated information
+    //console.log("Updated name:", name);
+    //console.log("Updated color:", color);
+    //update the list information here
+    const updatedList = { ...list, name: name, color: color };
+    changeList(updatedList).then(() => {
+        setIsEditModalOpen(false);
+        handleMoveList(updatedList);
+        }
+    );
+  };
+
+  return (
+    <View style={[styles.container, shadows.mediumShadow]}>
+      <BlurView
+        style={[
+          {
+            backgroundColor: hexToRgb(list.color, 0.3),
+            borderRadius: 5,
+          },
+        ]}
+      >
+        <View style={[styles.titler]}>
+          <Text style={styles.text}>{list.name}</Text>
         </View>
-    )
+        <View style={[styles.container, shadows.mediumShadow]}>
+          <Button title="Delete List" onPress={handleDeleteList} />
+          <Button title="Edit List" onPress={handleEditModalOpen} />
+        </View>
+        <ScrollView style={styles.carder}>
+          {cards.map((card) => (
+            <CardButton
+              key={card.id}
+              info={card}
+              onDelete={handleDelete}
+            />
+          ))}
+
+          <View style={styles.adder}>
+            <Button
+              style={styles.butRad}
+              title="Add Task"
+              onPress={() => {
+                setIsAddModalOpen(true);
+              }}
+            />
+          </View>
+        </ScrollView>
+        <AddCardModal
+          isOpen={isAddModalOpen}
+          closeModal={() => setIsAddModalOpen(false)}
+          onModalClose={(name, description) => {
+            addTaskAndGetTasks({
+              name: name,
+              description: description,
+              listId: list.id,
+            });
+          }}
+        />
+        <EditListModal
+          isOpen={isEditModalOpen}
+          closeModal={() => setIsEditModalOpen(false)}
+          onModalClose={handleEditModalClose}
+          list={list}
+        />
+      </BlurView>
+    </View>
+  );
 }
 
-export default List
+export default List;
--- Toodler/src/components/Modals/EditListModal/index.js
+++ Toodler/src/components/Modals/EditListModal/index.js
@@ -1,85 +1,56 @@
-import React, { useState, useEffect } from "react"
-import { TextInput, 
-    SafeAreaView, 
-    Button, 
-    TouchableOpacity, 
-    Text, 
-    View 
-} from "react-native"
-import Modal from "../../Modal"
-import styles from "./styles"
+import React, { useState } from "react";
+import { TextInput, SafeAreaView, Button, TouchableOpacity, Text, View } from "react-native";
+import Modal from "../../Modal";
+import styles from "./styles";
 import { Picker } from '@react-native-picker/picker';
-import { colors } from "../../../styles/colors"
-import { shadows } from "../../../styles/shadows"
+import { colors } from "../../../styles/colors";
+import { shadows } from "../../../styles/shadows";
 
 const EditListModal = ({ isOpen, closeModal, onModalClose, list }) => {
-    const [name, onChangeName] = useState(list.name)
-    const [color, onChangeColor] = useState(list.color)
-    const [deleted, setDeleted] = useState(false)//?
-    const [isPickerVisible, setPickerVisible] = useState(false)
+  const [name, setName] = useState(list.name);
+  const [color, setColor] = useState(list.color);
+  const [isPickerVisible, setPickerVisible] = useState(false);
+  const inputStyle = [styles.input, shadows.smallShadow];
+  const [colorName, setColorName] = useState("");
 
-    const inputStyle = [styles.input, shadows.smallShadow]
-    const [colorName, setColorName] = useState("");
+  const handleClose = () => {
+    onModalClose(name, color);
+    closeModal();
+    setName(""); // Clear the name input
+    setColor(""); // Clear the color input
+  };
 
-    const handleClose = () => {
-        if (name === "") {
-            onModalClose(name, color, deleted)
-            onChangeName("")
-            onChangeColor("")
-            closeModal()
-        } else {
-            onModalClose(name, color, deleted)
-            closeModal()
-            onChangeName("") // clear the name input
-            onChangeColor("") // clear the description input
-        }
-    }
+  return (
+    <Modal isOpen={isOpen} closeModal={handleClose} title="Edit">
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={setName}
+          placeholder="List Name"
+          placeholderTextColor={colors.grey}
+          value={name}
+        />
+        <TouchableOpacity onPress={() => setPickerVisible(true)}>
+          <Text style={inputStyle}>{colorName || "Color"}</Text>
+        </TouchableOpacity>
+        {isPickerVisible && (
+          <Picker
+            selectedValue={color}
+            onValueChange={setColor}
+          >
+            {Object.keys(colors).map((key) => (
+              <Picker.Item label={key} value={colors[key]} key={key} />
+            ))}
+          </Picker>
+        )}
+        <Button
+          style={styles.input}
+          title="Accept"
+          onPress={handleClose}
+        />
+      </SafeAreaView>
+    </Modal>
+  );
+};
 
-    useEffect(() => {
-        if (deleted) {
-            handleClose()
-        }
-    }, [deleted])
-
-    return (
-        <Modal isOpen={isOpen} closeModal={handleClose} title="Edit">
-            <SafeAreaView>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={onChangeName}
-                    placeholder="List Name"
-                    placeholderTextColor={colors.grey}
-                    value={name}
-                />
-                <TouchableOpacity onPress={() => setPickerVisible(true)}>
-                    <Text style={inputStyle}>{colorName || "Color"}</Text>
-                </TouchableOpacity>
-                {isPickerVisible && (
-                    <Picker
-                        selectedValue={color}
-                        onValueChange={onChangeColor}
-                    >
-                        {Object.keys(colors).map((key) => (
-                            <Picker.Item label={key} value={colors[key]} key={key} />
-                        ))}
-                    </Picker>
-                )}
-                <Button
-                    style={styles.input}
-                    title="Accept"
-                    onPress={handleClose}
-                />
-                <Button
-                    style={styles.input}
-                    title="Delete"
-                    onPress={() => {
-                        setDeleted(true)
-                    }}
-                />
-            </SafeAreaView>
-
-        </Modal>
-    )
-}
-
-export default EditListModal
+export default EditListModal;
