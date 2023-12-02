081242a4257c49dcd3ba0627e542aff6b49c2899
Ingik21 <ingik21@ru.is>
2023-12-02 12:48:25 +0000

    last

--- Toodler/src/components/Modals/EditListModal/index.js
+++ Toodler/src/components/Modals/EditListModal/index.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react"
-import { TextInput, SafeAreaView, Button, TouchableOpacity, Text } from "react-native"
+import { TextInput, 
+    SafeAreaView, 
+    Button, 
+    TouchableOpacity, 
+    Text, 
+    View 
+} from "react-native"
 import Modal from "../../Modal"
 import styles from "./styles"
 import { Picker } from '@react-native-picker/picker';
@@ -7,51 +13,33 @@ import { colors } from "../../../styles/colors"
 import { shadows } from "../../../styles/shadows"
 
 const EditListModal = ({ isOpen, closeModal, onModalClose, list }) => {
-    const [name, setName] = useState("")
-    const [color, setColor] = useState("")
+    const [name, onChangeName] = useState(list.name)
+    const [color, onChangeColor] = useState(list.color)
     const [deleted, setDeleted] = useState(false)//?
     const [isPickerVisible, setPickerVisible] = useState(false)
+
     const inputStyle = [styles.input, shadows.smallShadow]
     const [colorName, setColorName] = useState("");
-    const onChangeName = (text) => {
-        setName(text);
-      };
-    const onChangeColor = (value) => {
-        setColor(value);
-        setColorName(Object.keys(colors)[Object.values(colors).indexOf(value)]);
-      };
-
-    useEffect(() => {
-        if (deleted) {
-            setDeleted(false)
-            handleCloseReturn()
-        }
-    }, [deleted])
-
-    useEffect(() => {
-        if (list) {
-            onChangeName(list.name)
-            onChangeColor(list.color)
-        }
-    }, [list])
 
     const handleClose = () => {
-        setName("");
-        setColor("");
-        closeModal();
-      };
-
-      const handleCloseReturn = () => {
-        if (deleted) {
-          onModalClose(name, color, deleted);
+        if (name === "") {
+            onModalClose(name, color, deleted)
+            onChangeName("")
+            onChangeColor("")
+            closeModal()
         } else {
-        if (name && color) {
-          onModalClose(name, color);
+            onModalClose(name, color, deleted)
+            closeModal()
+            onChangeName("") // clear the name input
+            onChangeColor("") // clear the description input
         }
-        setName("");
-        setColor("");
-        closeModal();}
-      };
+    }
+
+    useEffect(() => {
+        if (deleted) {
+            handleClose()
+        }
+    }, [deleted])
 
     return (
         <Modal isOpen={isOpen} closeModal={handleClose} title="Edit">
@@ -79,7 +67,7 @@ const EditListModal = ({ isOpen, closeModal, onModalClose, list }) => {
                 <Button
                     style={styles.input}
                     title="Accept"
-                    onPress={() => handleCloseReturn(name, color)}
+                    onPress={handleClose}
                 />
                 <Button
                     style={styles.input}
@@ -89,8 +77,92 @@ const EditListModal = ({ isOpen, closeModal, onModalClose, list }) => {
                     }}
                 />
             </SafeAreaView>
+
         </Modal>
     )
 }
 
 export default EditListModal
+
+
+
+//     const onChangeName = (text) => {
+//         setName(text);
+//       };
+//     const onChangeColor = (value) => {
+//         setColor(value);
+//         setColorName(Object.keys(colors)[Object.values(colors).indexOf(value)]);
+//       };
+
+//     useEffect(() => {
+//         if (deleted) {
+//             setDeleted(false)
+//             handleCloseReturn()
+//         }
+//     }, [deleted])
+
+//     useEffect(() => {
+//         if (list) {
+//             onChangeName(list.name)
+//             onChangeColor(list.color)
+//         }
+//     }, [list])
+
+//     const handleClose = () => {
+//         setName("");
+//         setColor("");
+//         closeModal();
+//       };
+
+//       const handleCloseReturn = () => {
+//         if (deleted) {
+//           onModalClose(name, color, deleted);
+//         } else {
+//         if (name && color) {
+//           onModalClose(name, color);
+//         }
+//         setName("");
+//         setColor("");
+//         closeModal();}
+//       };
+
+//     return (
+//         <Modal isOpen={isOpen} closeModal={handleClose} title="Edit">
+//             <SafeAreaView>
+//                 <TextInput
+//                     style={styles.input}
+//                     onChangeText={onChangeName}
+//                     placeholder="List Name"
+//                     placeholderTextColor={colors.grey}
+//                     value={name}
+//                 />
+//                 <TouchableOpacity onPress={() => setPickerVisible(true)}>
+//                     <Text style={inputStyle}>{colorName || "Color"}</Text>
+//                 </TouchableOpacity>
+//                 {isPickerVisible && (
+//                     <Picker
+//                         selectedValue={color}
+//                         onValueChange={onChangeColor}
+//                     >
+//                         {Object.keys(colors).map((key) => (
+//                             <Picker.Item label={key} value={colors[key]} key={key} />
+//                         ))}
+//                     </Picker>
+//                 )}
+//                 <Button
+//                     style={styles.input}
+//                     title="Accept"
+//                     onPress={() => handleCloseReturn(name, color)}
+//                 />
+//                 <Button
+//                     style={styles.input}
+//                     title="Delete"
+//                     onPress={() => {
+//                         setDeleted(true)
+//                     }}
+//                 />
+//             </SafeAreaView>
+//         </Modal>
+//     )
+// }
+
