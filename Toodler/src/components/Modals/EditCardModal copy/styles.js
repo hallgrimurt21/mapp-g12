8b046e61963d0626252445cfac0e8044448e7eb7
Ingik21 <ingik21@ru.is>
2023-12-02 14:58:32 +0000

    I Promise

---
+++ Toodler/src/components/Modals/EditCardModal copy/styles.js
@@ -0,0 +1,98 @@
+import { StyleSheet } from "react-native"
+import React from "react"
+import { deviceWidth } from "../../../styles/deviceWidth"
+
+export default StyleSheet.create({
+    input: {
+        height: 40,
+        marginBottom: 12,
+        borderWidth: 1,
+        textAlign: "left",
+        width: "100%",
+        padding: 10,
+        borderRadius: 4,
+    },
+    modal: {
+        width: Math.min(deviceWidth * 0.98, 400),
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 15,
+        backgroundColor: "white",
+        borderRadius: 8,
+        padding: 15,
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 0, // These can't both be 0
+            height: 1, // i.e. the shadow has to be offset in some way
+        },
+    },
+
+    Button: {
+        width: Math.min(deviceWidth * 0.5, 150),
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 4,
+        alignSelf: "flex-start",
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 0, // These can't both be 0
+            height: 1, // i.e. the shadow has to be offset in some way
+        },
+    },
+    title: {
+        marginTop: 15,
+        width: Math.min(deviceWidth * 0.98, 400),
+        backgroundColor: "white",
+        borderRadius: 8,
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 0, // These can't both be 0
+            height: 1, // i.e. the shadow has to be offset in some way
+        },
+    },
+
+    titleText: {
+        padding: 10,
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    insider: {
+        padding: 20,
+        width: "100%",
+    },
+    deleteButton: {
+        width: Math.min(deviceWidth * 0.5, 150),
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: "red",
+        alignSelf: "flex-start",
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 0, // These can't both be 0
+            height: 1, // i.e. the shadow has to be offset in some way
+        },
+    },
+    deleteText: {
+        color: "red",
+    },
+    buttonPart: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+})
